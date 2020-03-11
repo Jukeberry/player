a9466d8496e11647c8a7bb8314e6d3b5506a8df7
nohorbee <nohorbee+github@gmail.com>
2020-03-11 20:21:04 +0000

    Adding chance of connecting with a MQTT to inform the card/presented event

--- mvp.py
+++ mvp.py
@@ -4,7 +4,10 @@
 import pygame
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-
+import paho.mqtt.client as mqtt
+import datetime
+import json
+import time
 
 def song(i):
     switcher={
@@ -15,22 +18,32 @@ def song(i):
     }
     return switcher.get(i,"INVALID");
 
+selectedDevice = False
 reader = SimpleMFRC522()
+mqttClient = mqtt.Client("jukeberry")
+mqttClient.connect("192.168.0.12", 1883)
 try:
     pygame.mixer.init()
     pygame.mixer.music.set_volume(1.0)
     while(True):
         id, text = reader.read()
+        now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%zZ")
         print("id:" + str(id))
         print("text:" + text)
         
+        
         play = song(id)
         print(play)
-        pygame.mixer.music.load(play)
-        pygame.mixer.music.play()
-    
-    
-    
+        if(selectedDevice):
+            print("playing in raspi")
+            pygame.mixer.music.load(play)
+            pygame.mixer.music.play()
+        else:
+            message = {"id": str(id), "text": text, "sentAt": now }
+            mqttClient.publish("card/presented", json.dumps(message))
+        
+        time.sleep(1);
+        
     
 finally:
     GPIO.cleanup()
