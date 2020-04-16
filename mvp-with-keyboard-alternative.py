9c4e3acad11c2bb3717990e3dc3cd6121624a918
nohorbee <nohorbee+github@gmail.com>
2020-04-16 03:44:59 +0000

    Adding a working example that can make use of the keyboard to emulate the cards

---
+++ mvp-with-keyboard-alternative.py
@@ -0,0 +1,93 @@
+
+
+#!/usr/bin/env python
+##import pygame
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import paho.mqtt.client as mqtt
+import datetime
+import json
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+print("Booting Jukeberry")
+
+rfidBased = True
+
+def stop(pin):
+    now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%zZ")
+    message = {"sentAt": now }
+    print (message)
+    mqttClient.publish("stop/pushed", json.dumps(message))
+    
+def play(id, text):
+    now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%zZ")
+    message = {"id": str(id), "text": text, "sentAt": now }
+    mqttClient.publish("card/presented", json.dumps(message))
+
+def keyboardRead():
+    i=1
+    switcher=[
+        (96675065265, "Bodysnatchers"),
+        (664003290249, "Creep"),
+        (649586696873, "High and Dry")
+    ]
+    for item in switcher:
+        print(str(i) + ": " + str(item))
+        i = i+1
+    text = input('Select option:')
+    if text.isnumeric() and int(text) >=1 and int(text) <=3:
+        return switcher[int(text)-1]
+    else:
+        print("Invalid Option")
+        return keyboardRead()
+
+def song(i):
+    switcher={
+        96675065265: "music/ParanoidAndroid.mp3",
+        664003290249: "music/Creep.mp3",
+        649586696873: "music/HighAndDry.mp3",
+    }
+    return switcher.get(i,"INVALID");
+
+mqttTransport = "websockets"
+mqttHost = "mr1nljqp0y1g2d.messaging.solace.cloud"
+mqttPort = 21128
+mqttUser = "solace-cloud-client"
+mqttPassword = "7ide7v7illr1fs4bsio3ls26f0"
+
+mqttClient = mqtt.Client("jukeberry", transport=mqttTransport)
+mqttClient.username_pw_set(username="solace-cloud-client",password="7ide7v7illr1fs4bsio3ls26f0")
+mqttClient.connect(mqttHost, mqttPort)
+
+print("Connected to: " + mqttHost)
+
+GPIO.add_event_detect(11, GPIO.FALLING, callback=stop, bouncetime=300)
+print("Stop button registered") 
+
+if rfidBased:
+    reader = SimpleMFRC522()
+    print("Enabling RFID Reader")
+else:
+    print("Enabling Keyboard RFID Reader emulation")
+
+try:
+    while(True):
+        if rfidBased:
+            id, text = reader.read()
+        else:
+            id, text = keyboardRead()
+        
+        print("Read ID:" + str(id))
+        print("Card Song:" + str(text))
+        
+        play(id, text)
+                
+        if rfidBased:
+            time.sleep(1);
+except KeyboardInterrupt:         
+    GPIO.cleanup()
+    raise
+finally:
+    GPIO.cleanup()
--- mvp.py
+++ mvp.py
@@ -1,13 +1,15 @@
 
 
 #!/usr/bin/env python
-import pygame
+##import pygame
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import paho.mqtt.client as mqtt
-import datetime
-import json
+##import paho.mqtt.client as mqtt
+##import datetime
+##import json
 import time
+
+GPIO.setmode(GPIO.BCM)
 
 def song(i):
     switcher={
@@ -17,35 +19,45 @@ def song(i):
         711986787821: "music/BonfireHeart.mp3"
     }
     return switcher.get(i,"INVALID");
+##if(False): 
+##    mqttTransport = "websockets"
+##    mqttHost = "mr1nljqp0y1g2d.messaging.solace.cloud"
+##    mqttPort = 21128
+##    mqttUser = "solace-cloud-client"
+##    mqttPassword = "7ide7v7illr1fs4bsio3ls26f0"
+##else:
+##    mqttTransport = "tcp"
+##    mqttHost = "192.168.0.12"
+##    mqttPort = 1883
 
 selectedDevice = False
 reader = SimpleMFRC522()
-mqttClient = mqtt.Client("jukeberry")
-mqttClient.connect("192.168.0.12", 1883)
+##mqttClient = mqtt.Client("jukeberry", transport=mqttTransport)
+##mqttClient.username_pw_set(username="solace-cloud-client",password="7ide7v7illr1fs4bsio3ls26f0")
+##mqttClient.connect(mqttHost, mqttPort)
 try:
-    pygame.mixer.init()
-    pygame.mixer.music.set_volume(1.0)
+##    pygame.mixer.init()
+##    pygame.mixer.music.set_volume(1.0)
     while(True):
         id, text = reader.read()
-        now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%zZ")
+##        now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%zZ")
         print("id:" + str(id))
-        print("text:" + text)
+        print("text:" + str(text))
         
         
-        play = song(id)
-        print(play)
-        if(selectedDevice):
-            print("playing in raspi")
-            pygame.mixer.music.load(play)
-            pygame.mixer.music.play()
-        else:
-            message = {"id": str(id), "text": text, "sentAt": now }
-            mqttClient.publish("card/presented", json.dumps(message))
+##        play = song(id)
+##        print(play)
+##        if(selectedDevice):
+##            print("playing in raspi")
+##            pygame.mixer.music.load(play)
+##            pygame.mixer.music.play()
+##        else:
+##            message = {"id": str(id), "text": text, "sentAt": now }
+##            mqttClient.publish("card/presented", json.dumps(message))
         
         time.sleep(1);
-        
-    
+except KeyboardInterrupt:         
+    GPIO.cleanup()
+    raise
 finally:
     GPIO.cleanup()
-
-
